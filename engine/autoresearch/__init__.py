070baa50718f06cdc65f52f66d3ae59ed21b19ab
YuFei Chi <2024111040809@stu.hbnu.edu.cn>
2026-09-03 23:33:32 +0800

    feat: expose evidence autoresearch API

---
+++ engine/autoresearch/__init__.py
@@ -0,0 +1,5 @@
+from .controller import EvidenceAutoresearchController, StepResult
+from .contracts import *
+from .gap_priority import *
+from .research_memory import ResearchMemory
+from .saturation import *
