e386a626b4391fe32d381c768015200a16ef5848
YuFei Chi <2024111040809@stu.hbnu.edu.cn>
2026-09-04 09:50:14 +0800

    feat: export atomic autoresearch commit helpers

--- engine/autoresearch/__init__.py
+++ engine/autoresearch/__init__.py
@@ -1,5 +1,6 @@
 from .controller import EvidenceAutoresearchController, StepResult
 from .contracts import *
+from .commit import build_append_only_mutation, commit_staging_bundle
 from .gap_priority import *
 from .research_memory import ResearchMemory
 from .saturation import *
